2e5d14ec73ed43546bd4b35e66467dae0dd6f98f
gabrpavel <gabrpavel@gmail.com>
2023-02-16 19:32:53 +0300

    lab9

---
+++ lab9/main.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+
+int main(void) {
+
+    int size = 1;
+    size = get_size(size);
+    Film* films = memory(size);
+
+
+    create_mas_struct(films, size);
+    show_mas_struct(films, size);
+    free_memory(films, size);
+
+
+    getch();
+    return 0;
+}
